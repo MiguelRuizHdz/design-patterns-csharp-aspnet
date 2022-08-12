a45af0fbb70013b3ae28c70296c7053c60cb1ed9
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-08-12 12:49:36 -0500

    Clases y Objetos

---
+++ IntroduccionDesignPatterns/IntroduccionDesignPatterns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IntroduccionDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person miguel = new Person("Miguel", 21, "Mx");
+            Person pancho = new Person("Francisco", 21, "Mx");
+            Console.WriteLine(miguel.Show());
+            Console.WriteLine(pancho.Show());
+        }
+    }
+
+    class Person
+    {
+        public string name;
+        public int age;
+        public string nationality;
+
+        public Person(string name_, int age_, string nationality_)
+        {
+            this.name = name_;
+            this.age = age_;
+            this.nationality = nationality_;
+        }
+
+        public string Show()
+        {
+            return name + " " + age + " " + nationality;
+        }
+    }
+}
