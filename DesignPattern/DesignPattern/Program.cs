2e2d9a2ef2362cf6e30fc99586f765ac0d84c3b6
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-08-15 09:49:56 -0500

    Factory Method: Explicación

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPattern.FactoryPattern;
+using System;
 
 namespace DesignPattern
 {
@@ -6,16 +7,34 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            var singleton = Singleton.Singleton.Instance;
-            var log = Singleton.Log.Instance; // se obtiene el objeto
-            log.Save("a");
-            log.Save("b");
-            
-            // El objeto es el mismo en toda la aplicación
-            var a = Singleton.Singleton.Instance;
-            var b = Singleton.Singleton.Instance;
+            /*
+             * Factory Method
+             */
 
-            Console.WriteLine(a == b); // True
+            // no puedes crear un objeto de una clase abstracta pero si puedes crearlo de sus jerarquías o sea sus hijos.
+            SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+            SaleFactory internetSaleFactory = new StoreSaleFactory(2);
+
+            ISale sale1 = storeSaleFactory.GetSale();
+            sale1.Sell(15);
+
+            ISale sale2 = internetSaleFactory.GetSale();
+            sale2.Sell(15);
+
+
+            /*
+             * Singleton
+             */
+            //var singleton = Singleton.Singleton.Instance;
+            //var log = Singleton.Log.Instance; // se obtiene el objeto
+            //log.Save("a");
+            //log.Save("b");
+            
+            //// El objeto es el mismo en toda la aplicación
+            //var a = Singleton.Singleton.Instance;
+            //var b = Singleton.Singleton.Instance;
+
+            //Console.WriteLine(a == b); // True
         }
     }
 }
