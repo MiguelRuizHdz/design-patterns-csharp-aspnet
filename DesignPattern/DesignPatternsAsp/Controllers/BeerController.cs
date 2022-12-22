0e41d063b54d2cd4c14b1f70dc5e1ff3b3981bcc
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-12-22 09:30:13 -0600

    Unit Of Work: Guardando en 2 tablas

--- DesignPattern/DesignPatternsAsp/Controllers/BeerController.cs
+++ DesignPattern/DesignPatternsAsp/Controllers/BeerController.cs
@@ -1,9 +1,13 @@
-﻿using DesignPatterns.Repository;
+﻿using DesignPatterns.Models.Data;
+using DesignPatterns.Repository;
 using DesignPatternsAsp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace DesignPatternsAsp.Controllers
 {
@@ -26,6 +30,50 @@ namespace DesignPatternsAsp.Controllers
                                                    Style = d.Style
                                                };
             return View("Index", beers);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var brands = _unitOfWork.Brands.Get();
+            ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FormBeerViewModel beerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var brands = _unitOfWork.Brands.Get();
+                ViewBag.Brands = new SelectList(brands, "BrandId", "Name");
+
+                return View("Add", beerViewModel);
+            }
+
+            var beer = new Beer();
+            beer.Name = beerViewModel.Name;
+            beer.Style = beerViewModel.Style;
+
+            if (beerViewModel.BrandId == null)
+            {
+                var brand = new Brand();
+                brand.Name = beerViewModel.OtherBrand;
+                brand.BrandId = Guid.NewGuid();
+                beer.BrandId = brand.BrandId;
+                _unitOfWork.Brands.Add(brand);
+            }
+            else
+            {
+                beer.BrandId = (Guid)beerViewModel.BrandId;
+            }
+
+            _unitOfWork.Beers.Add(beer);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
